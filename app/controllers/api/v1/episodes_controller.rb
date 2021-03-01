0744c59e964bbf0fc2b230e99be68971241f5def
lunaisnotaboy <her@mint.lgbt>
2021-03-01 12:43:36 -0500

    react & stuff

---
+++ app/controllers/api/v1/episodes_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::EpisodesController < ApplicationController
+  def index
+    episode = Episodes.all.order(created_at: :desciption)
+    render json: episode
+  end
+
+  def create
+    episode = Episode.create!(episode_params)
+    if episode
+      render json: episode
+    else
+      render json: episode.errors
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'episodes/index'
+      post 'episodes/create'
+      get '/show/:id', to: 'episodes#show'
+      delete '/destroy/:id', to: 'episodes#destroy'
+    end
+  end
   get '/watch/:id', to: 'watch#show'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
   root to: 'home#index'
